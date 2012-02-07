705109deac2ac4fcda9f73b6e75f41189c94218f
Joe Kurleto <joedoku@gmail.com>
2012-02-07 11:11:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cliqs::Application.config.secret_token = 'e8f2c5a56951f44c7c3987a81e912420eff80226d4e420e0d12d74d06dd4021d9b7a0c8edc60102d408d4392c46421f5302c93592581544efd75bd111cc5a3dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cliqs::Application.config.session_store :cookie_store, :key => '_cliqs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cliqs::Application.config.session_store :active_record_store
