bcf7e76984d71d47b3930833e89a3074d14e328f
Joe Kurleto <joedoku@gmail.com>
2012-02-19 13:56:15 -0500

    photos on cliq wall. still need to add commenting functionality

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  #process :resize_to_fit => [600, 600]
+  process :resize_to_fit => [600, 600]
   #
   # def scale(width, height)
   #   # do something
@@ -34,7 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :wall do
-    process :resize_to_fit => [386, 250]
+    process :resize_to_fit => [380, 300]
+  end
+  version :square do
+    process :resize_to_fill => [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
