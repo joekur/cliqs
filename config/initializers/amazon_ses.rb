874e26df112149e98a7c02efab941b2491c7d650
Joe Kurleto <joedoku@gmail.com>
2012-03-25 21:53:23 -0400

    commented out code configuring ses mailer

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,5 +1,5 @@
 # extend ActionMailer
-ActionMailer::Base.custom_amazon_ses_mailer = AWS::SES::Base.new({
-   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-})
+#ActionMailer::Base.custom_amazon_ses_mailer = AWS::SES::Base.new({
+#   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+#})
